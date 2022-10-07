def6aeaa8d82f685b20914170eef8a762a761d64
Sergittko <derov180214@gmail.com>
2022-10-08 00:56:10 +0300

    work on task input

--- src/components/MainContent/TaskInput/TaskInput.jsx
+++ src/components/MainContent/TaskInput/TaskInput.jsx
@@ -2,12 +2,13 @@ import style from "./TaskInput.module.scss";
 
 const TaskInput = () => {
   return (
-    <div>
-      <input type="text" />
+    <div className={style.input_container}>
+      <input type="text" placeholder="Type task" />
       <select name="user_profile_color_1">
         <option value="1">Work</option>
         <option value="2">No Section</option>
       </select>
+      <button>Submit</button>
     </div>
   );
 };

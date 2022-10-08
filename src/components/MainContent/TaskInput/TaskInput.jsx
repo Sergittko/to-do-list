a1263abe372c3dadea5042e514c0d73cf8396dfa
Sergittko <derov180214@gmail.com>
2022-10-08 17:06:26 +0300

    work on form

--- src/components/MainContent/TaskInput/TaskInput.jsx
+++ src/components/MainContent/TaskInput/TaskInput.jsx
@@ -1,15 +1,32 @@
 import style from "./TaskInput.module.scss";
-
+import { useState } from "react";
 const TaskInput = () => {
+  let [selectState, changeSelect] = useState('No Section');
+  let [textState, changeText] = useState("");
+
+  let handleSubmit = (event) => {
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    changeSelect('No Section');
+    changeText('');
+    event.preventDefault();
+  };
   return (
-    <div className={style.input_container}>
-      <input type="text" placeholder="Type task" />
-      <select name="user_profile_color_1">
-        <option value="1">Work</option>
-        <option value="2">No Section</option>
+    <form className={style.input_container} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Type task"
+        value={textState}
+        onChange={(e) => changeText(e.target.value)}
+      />
+      <select name="user_profile_color_1"
+              value={selectState}
+              onChange={(e) => changeSelect(e.target.value)}>
+        <option value="Work">Work</option>
+        <option value="No Section">No Section</option>
       </select>
-      <button>Submit</button>
-    </div>
+      <input type="submit" value="Submit" />
+    </form>
   );
 };
 

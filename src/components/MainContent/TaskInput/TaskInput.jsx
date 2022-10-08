573f7979ba5ea110e2155f596b0d74088343c674
Sergittko <derov180214@gmail.com>
2022-10-08 17:06:47 +0300

    work on form

--- src/components/MainContent/TaskInput/TaskInput.jsx
+++ src/components/MainContent/TaskInput/TaskInput.jsx
@@ -1,14 +1,14 @@
 import style from "./TaskInput.module.scss";
 import { useState } from "react";
 const TaskInput = () => {
-  let [selectState, changeSelect] = useState('No Section');
+  let [selectState, changeSelect] = useState("No Section");
   let [textState, changeText] = useState("");
 
   let handleSubmit = (event) => {
     console.log(event.target[0].value);
     console.log(event.target[1].value);
-    changeSelect('No Section');
-    changeText('');
+    changeSelect("No Section");
+    changeText("");
     event.preventDefault();
   };
   return (
@@ -19,9 +19,11 @@ const TaskInput = () => {
         value={textState}
         onChange={(e) => changeText(e.target.value)}
       />
-      <select name="user_profile_color_1"
-              value={selectState}
-              onChange={(e) => changeSelect(e.target.value)}>
+      <select
+        name="user_profile_color_1"
+        value={selectState}
+        onChange={(e) => changeSelect(e.target.value)}
+      >
         <option value="Work">Work</option>
         <option value="No Section">No Section</option>
       </select>
